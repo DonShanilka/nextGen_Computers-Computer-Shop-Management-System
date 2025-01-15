1ce3c4383ec48aefc5800e3c3b357bd14808c7fc
DonShanilka <shanilka799@gmail.com>
2025-01-15 19:00:18 +0530

    check issuse

--- src/main/java/lk/ijse/Launcher.java
+++ src/main/java/lk/ijse/Launcher.java
@@ -15,7 +15,7 @@ public class Launcher extends Application {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashBord.fxml"))));
         stage.centerOnScreen();
         stage.setTitle("DashBoard");
-        System.out.println("Hello");
+        System.out.println("Hello Shanilka");
         stage.show();
     }
 }
